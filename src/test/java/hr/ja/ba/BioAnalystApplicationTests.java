1f6d5d32b60903aeff03e25742547bf84d4564f0
jdiminic <jdiminic@gmail.com>
2025-11-09 09:26:44 +0100

    Initial commit

---
+++ src/test/java/hr/ja/ba/BioAnalystApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.ja.ba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioAnalystApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
